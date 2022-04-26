154f54afa5236482720dfa6c118e1097f9f50f55
miloweller1 <59585004+miloweller1@users.noreply.github.com>
2022-04-25 22:23:33 -0700

    working

--- R/give_candygrams.R
+++ R/give_candygrams.R
@@ -31,7 +31,7 @@ give_candygrams <- function(person, number,
   number <- str_to_title(as.english(number))
 
 
-  glue::glue("{number} for {person}.")
+  glue::glue("{number} for {person}. {extra_message}")
 
 
 
